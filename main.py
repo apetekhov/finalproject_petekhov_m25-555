5042410a4e0b221d31d322d95eef9c6e34106771
Artem Petekhov <apetekhov@gmail.com>
2025-11-02 22:18:02 +0400

    Перенес main.py в корень проекта для корректного запуска приложения

---
+++ main.py
@@ -0,0 +1,11 @@
+from valutatrade_hub.cli.interface import run_app
+
+
+def main():
+    """Главная точка входа проекта ValutaTrade."""
+    print("ValutaTrade запущен.")
+    run_app()
+
+
+if __name__ == "__main__":
+    main()
--- valutatrade_hub/main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-def main() -> None:
-    print("ValutaTrade Hub is running!")
-
-
-if __name__ == "__main__":
-    main()
